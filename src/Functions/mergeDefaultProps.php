1ee7ace1d3c8a36fa5fd97853741a6f77b26bc54
Martin Adamko <attitude@users.noreply.github.com>
2020-11-06 02:07:29 +0100

    Add comment

--- src/Functions/mergeDefaultProps.php
+++ src/Functions/mergeDefaultProps.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * Merges Props with default props recursivelly
+ *
+ * - every key tthat is missing in Props will be merged
+ * - if property on the Props is a array then the defaults are merged recursivelly
+ * - every empty `array` turns to null
+ *
+ * @param array $props Passed by refference
+ * @param array $defaultProps
+ * @return void
+ */
 function mergeDefaultProps(array &$props, array $defaultProps) {
   foreach ($defaultProps as $key => $value) {
     if (array_key_exists($key, $props)) {
